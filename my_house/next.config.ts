e4406d89fd0f26c0b2fb16fe98fe6d414f2efe75
Antonin <antonin.femenias@epitech.eu>
2025-04-04 11:55:11 +0200

    WIP

--- my_house/next.config.ts
+++ my_house/next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
-  output: 'export',
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/your-repository-name/' : '',
+  basePath: isProd ? '/your-repository-name' : '',
+  output: 'export'
 };
 
 export default nextConfig;
