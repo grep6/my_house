00cdc4e91d1c2dd4734b9f3ccca7b027cea2ceb4
Antonin <antonin.femenias@epitech.eu>
2025-04-05 09:46:05 +0200

    param depoiment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
+const isDeploy = process.env.NEXT_PUBLIC_DEPLOY === 'true';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/my_house/' : '',
+  assetPrefix: isProd ? '/my_house/' : isDeploy ? '/my_house/' : '',
   basePath: isProd ? '/my_house' : '',
   output: 'export'
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+// import {BrowserRouter} from "next/navigation";
 
 export default function Home() {
   return (
